19fb6006fd2821fd96cee6de00df22b654c04b64
Manuel Vilela Blanco <vilelablancomanuel@gmail.com>
2022-09-08 21:53:43 -0300

    Proyecto webDjango

---
+++ webDjango/view.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def home(self, name):
+    return HttpResponse(f"Hola soy {name}")
+
+def homePage(self):
+    lista = [1,2,3,4,5]
+    data = {"nombre": "Manuel", "apellido": "Vilela", "lista" : lista}
+    planilla = loader.get_template("home.html")
+    documento = planilla.render(data)
+    return HttpResponse(documento)
